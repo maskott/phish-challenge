d48294509422cc434b2bfb51f70a20340ff6c3c3
Mike Skott <mike@skott.us>
2020-03-18 00:34:30 -0400

    switching to new awesomeness

--- src/services/PhishinService.js
+++ src/services/PhishinService.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://phish.in/api/v1",
+  baseURL: "http://phishin.skott.us/",
   timeout: 1000,
   headers: {
-    Accept: "application/json",
-    Authorization:
-      "Bearer 3781a190cb0140f485c533cf197ecee4e8e20e92385ed71e842f98c87c79d8d52043ef17088e8dbe3b25edf4401f8f93"
+    Accept: "application/json"
   }
 });
 
